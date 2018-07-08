01c197d7b65e5d48d6bd2b9f0faa13cfc3ae5c47
mike-seger <mss@unknown.com>
2018-07-08 13:47:57 +0200

    added post

--- src/main/java/com/github/spring/example/QRCodeServiceApplication.java
+++ src/main/java/com/github/spring/example/QRCodeServiceApplication.java
@@ -1,5 +1,6 @@
 package com.github.spring.example;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.*;
 import org.springframework.scheduling.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -24,7 +26,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class QRCodeServiceApplication {
 
 	static final String qrcodeENDPOINT = "/qrcode";
-
+	
 	@Autowired
 	private ImageService imageService;
 
@@ -38,21 +40,27 @@ public class QRCodeServiceApplication {
 		SpringApplication.run(QRCodeServiceApplication.class, args);
 	}
 
-	@RequestMapping(value = qrcodeENDPOINT, produces = MediaType.IMAGE_PNG_VALUE, method = {POST, GET})
-	public ResponseEntity<byte[]> getQRCode(
-			@RequestParam(value = "text") String text,
-			@RequestParam(value = "logoFile", required=false) String logoFile,
-			@RequestParam(value = "logo", required=false) byte [] logo
-	) {
-		//TODO use logo parameters to produce QR with embedded logo
+	@GetMapping(value = qrcodeENDPOINT, produces = MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<byte[]> qrCode(@RequestParam(value = "text") String text) {
 		try {
 			return ResponseEntity.ok().cacheControl(CacheControl.maxAge(maxCachedAgeMinutes, TimeUnit.MINUTES))
-				.body(imageService.generateQRCodeAsync(text, size, size).get());
+					.body(imageService.generateQRCodeAsync(text, size, size).get());
 		} catch (Exception ex) {
 			throw new InternalServerError("Error while generating QR code image.", ex);
 		}
 	}
-	
+
+	@PostMapping(value = qrcodeENDPOINT, produces = MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<byte[]> qrCode(@RequestParam(value = "file") MultipartFile file) {
+		try {
+			String text=new Scanner(file.getInputStream()).useDelimiter("\\A").next();
+			return ResponseEntity.ok().cacheControl(CacheControl.maxAge(maxCachedAgeMinutes, TimeUnit.MINUTES))
+					.body(imageService.generateQRCodeAsync(text, size, size).get());
+		} catch (Exception ex) {
+			throw new InternalServerError("Error while generating QR code image.", ex);
+		}
+	}
+
 	@Scheduled(fixedRate = maxCachedAgeMinutes*1000*60)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping(value = qrcodeENDPOINT)
